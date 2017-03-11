a783d1ed725279cedb533979661cd7823f84d080
spooledrotten <khanalm977@gmail.com>
2017-03-10 21:52:24 -0500

    first commit

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,4 +1,4 @@
-/*$(function() {
+$(function() {
 
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
@@ -65,40 +65,6 @@
 
 
 /*When clicking on Full hide fail/success boxes */
-/*$('#name').focus(function() {
+$('#name').focus(function() {
     $('#success').html('');
-});*/
-/*$(function() {
-    $("#contactForm").submit(function(e) {
-      e.preventDefault();
-      $.ajax({
-        url: "//formspree.io/khanalm977@gmail.com", 
-        method: "POST",
-        data: $(this).serialize(),
-        dataType: "json",
-        success: function(data){
-          // Success message
-          $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-              .append("</button>");
-          $('#success > .alert-success')
-              .append("<strong>Your message has been sent. </strong>");
-          $('#success > .alert-success')
-              .append('</div>');
-
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-        error: function(){
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-              .append("</button>");
-          $('#success > .alert-danger').append("<strong>Sorry it seems that my mail server is not responding. Please try again later!");
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        }
-      });
-    });
-});*/
+});
